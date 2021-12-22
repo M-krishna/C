6e1eb46b69954eb69a35475d279f9c65d42f0e14
M Krishna <krishnamurugan589@gmail.com>
2021-12-22 18:38:08 +0530

    some escape sequences

---
+++ escape_sequences.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+// some escape sequences
+
+int main() {
+	printf("Hey there!\n"); // \n is for new line
+	printf("Hello\t there!\t\n"); // \t is tab space
+	// if we want to include double quotes inside print statement
+	printf("\"This is a quote\" - someone\n");
+	// include single quote inside print statement
+	printf("\'This is a quote\'\n");
+}
